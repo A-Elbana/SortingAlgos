7de88be130f293024dc492c186fdaac058894a13
AElbana <manelbana079@gmail.com>
2025-01-25 14:03:21 +0200

    Implemented a more efficient Random Number Generator

--- classes.py
+++ classes.py
@@ -32,9 +32,10 @@ class Element(Rectangle):
 
 
 
+
+
+# Implementation using Durstenfeld-Fisher-Yates shuffle algorithm
 class RNGenerator:
-    
-    used_numbers = set()
     def __init__(self, max_value) -> None:
         """Generator class
 
@@ -44,17 +45,50 @@ class RNGenerator:
         """
         # Set max value of generator
         self.max_value = max_value
-        pass
+        self.numbers = list(range(1, max_value + 1))
+        self.index = max_value
 
     def next(self) -> int:
-        """Generates next unique random number.
+        """Generates next unique random number using Durstenfeld-Fisher-Yates method.
 
         Returns:
             `int`: A unique random number.
         """
-        while len(self.used_numbers) < self.max_value:
-            random_number = random.randint(1,self.max_value)
-            if random_number not in self.used_numbers:
-                self.used_numbers.add(random_number)
-                return random_number
+        if self.index == 0:
+            raise ValueError("No more unique numbers to generate.")
+        
+        random_index = random.randint(0, self.index - 1)
+        self.numbers[random_index], self.numbers[self.index - 1] = self.numbers[self.index - 1], self.numbers[random_index]
+        self.index -= 1
+        return self.numbers[self.index]
+    
 
+
+
+
+# # Inefficient random number generator
+# class RNGenerator:
+    
+#     used_numbers = set()
+#     def __init__(self, max_value) -> None:
+#         """Generator class
+
+#         Usage
+#         -
+#             Helps to iteratively generate a unique random number.
+#         """
+#         # Set max value of generator
+#         self.max_value = max_value
+#         pass
+
+#     def next(self) -> int:
+#         """Generates next unique random number.
+
+#         Returns:
+#             `int`: A unique random number.
+#         """
+#         while len(self.used_numbers) < self.max_value:
+#             random_number = random.randint(1,self.max_value)
+#             if random_number not in self.used_numbers:
+#                 self.used_numbers.add(random_number)
+#                 return random_number
